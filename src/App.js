ce1e12328947c91e742afd753cf2a3cce4cbae6d
Bogdan DAMIAN <bogdann.damian@gmail.com>
2021-02-22 22:09:35 +0100

    Update menu-items to navigate withRouter HOC.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+
 import './App.css';
 import HomePage from './page/homepage/homepage.component'
+
+const HatsPage = () => (
+    <div>
+        <h1>Hats Page</h1>
+    </div>
+);
+const NoMatch = () => (
+    <div>
+        <h1>Not found</h1>
+    </div>
+)
+
 function App() {
-  return (
-    <HomePage/>
-  );
+    return (
+        /**
+         *  render based on the url path only the components that are really needed
+         *  Switch -> first valid route found the component is rendered
+         *         -> nothing else it's rendered
+         */
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/hats' component={HatsPage}/>
+                <Route>
+                    <NoMatch />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
+
 import './menu-item.styles.scss'
 
 // functional comp. because we don't need to to use a state
 // destructuring  title from props
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size,history,linkUrl,match}) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div
             className='background-image'
             style={{
@@ -17,4 +19,8 @@ const MenuItem = ({title, imageUrl, size}) => (
         </div>
     </div>
 )
-export default MenuItem;
+// withRouter
+// --> HOC higher-order component function that modifies a component
+// -->  this way we have access to Router history (and more) and avoid properties drilling through
+// components that don't actually need them
+export default withRouter(MenuItem);
